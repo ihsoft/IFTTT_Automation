a43ef429dab518d62348dc05f8df5c202a98692f
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2023-06-03 21:16:19 -0700

    Update pause/resume tools

--- Source/Tools/PauseTool.cs
+++ Source/Tools/PauseTool.cs
@@ -1,116 +1,60 @@
-using System.Linq;
-using Bindito.Core;
+// Timberborn Mod: Automation
+// Author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
 using IFTTT_Automation.Utils;
-using TimberApi.ToolSystem;
-using Timberborn.BaseComponentSystem;
 using Timberborn.BlockSystem;
 using Timberborn.BuildingsBlocking;
-using Timberborn.EntityPanelSystem;
-using Timberborn.EntitySystem;
-using Timberborn.PrefabSystem;
-using Timberborn.ToolSystem;
-using UnityDev.LogUtils;
 using UnityEngine;
 
 namespace IFTTT_Automation.Tools {
 
 // ReSharper disable once ClassNeverInstantiated.Global
-sealed class PauseTool : AbstractAreaSelectionTool {
+sealed class PauseTool : AbstractLockingTool {
   #region Local fields and properties
-  const string StartSelectingPromptLoc = "IgorZ.Automation.Common.LockSelectTool.StartSelectingPrompt";
-  const string StartObjectSelectingPromptLoc = "IgorZ.Automation.Common.LockSelectTool.StartObjectSelectingPrompt";
-  const string SelectingOneObjectLoc = "IgorZ.Automation.Common.LockSelectTool.SelectingObject";
-  const string SelectingNObjectsLoc = "IgorZ.Automation.Common.LockSelectTool.SelectingNObjects";
   const string ToolHintLoc = "IgorZ.Automation.PauseTool.Hint";
-
-  string _selectingPrefabName;
-  string _selectingEntityNiceName;
-  EntityBadgeService _entityBadgeService;
   #endregion
   
   #region CustomTool overrides
   /// <inheritdoc/>
-  public override void InitializeTool(ToolGroup toolGroup, ToolSpecification toolSpecification) {
+  protected override void Initialize() {
     HighlightColor = ActionColor = Color.red;
     TileColor = SideColor = Color.white;
-    base.InitializeTool(toolGroup, toolSpecification);
-
     DescriptionHintSectionLoc = ToolHintLoc;
-  }
-
-  /// <inheritdoc/>
-  public override string WarningText() {
-    if (_selectingPrefabName != null) {
-      return SelectedObjects.Count == 1
-          ? Loc.T(SelectingOneObjectLoc, _selectingEntityNiceName)
-          : Loc.T(SelectingNObjectsLoc, _selectingEntityNiceName, SelectedObjects.Count(ObjectFilterExpression));
-    }
-    if (!InputService.IsShiftHeld || CursorOnUI || SelectionModeActive) {
-      return "";
-    }
-    if (GetCompatibleComponent(HighlightedBlockObject) != null) {
-      return Loc.T(StartObjectSelectingPromptLoc, GetEntityNiceName(HighlightedBlockObject));
-    }
-    return Loc.T(StartSelectingPromptLoc);
+    base.Initialize();
   }
   #endregion
 
   #region AbstractAreaSelectionTool overries
   /// <inheritdoc/>
   protected override bool ObjectFilterExpression(BlockObject blockObject) {
+    if (!base.ObjectFilterExpression(blockObject)) {
+      return false;
+    }
     var component = GetCompatibleComponent(blockObject);
-    return component != null
-        && !component.Paused
-        && (_selectingPrefabName == null || blockObject.GetComponentFast<Prefab>().IsNamed(_selectingPrefabName));
+    return component != null && !component.Paused;
   }
 
   /// <inheritdoc/>
   protected override void OnObjectAction(BlockObject blockObject) {
     blockObject.GetComponentFast<PausableBuilding>().Pause();
   }
+  #endregion
 
+  #region AbstractLockingTool overries
   /// <inheritdoc/>
-  protected override void OnSelectionModeChange(bool newMode) {
-    base.OnSelectionModeChange(newMode);
-    if (newMode) {
-      if (InputService.IsShiftHeld && GetCompatibleComponent(HighlightedBlockObject)) {
-        _selectingPrefabName = HighlightedBlockObject.GetComponentFast<Prefab>().Name;
-        _selectingEntityNiceName = GetEntityNiceName(HighlightedBlockObject);
-      }
-    } else {
-      _selectingPrefabName = null;
-      _selectingEntityNiceName = null;
-    }
+  protected override bool CheckCanLockOnComponent(BlockObject obj) {
+    return GetCompatibleComponent(obj) != null;
   }
   #endregion
 
-  #region Local methods
-  [Inject]
-  public void InjectDependencies(EntityBadgeService entityBadgeService) {
-    _entityBadgeService = entityBadgeService;
-  }
-
-  PausableBuilding GetCompatibleComponent(BlockObject obj) {
-    if (obj == null) {
-      return null;
-    }
+  #region Implementation
+  static PausableBuilding GetCompatibleComponent(BlockObject obj) {
     var component = obj.GetComponentFast<PausableBuilding>();
     if (component != null && component.enabled && component.IsPausable()) {
       return component;
     }
     return null;
-  }
-
-  /// <summary>Returns a user friendly localized name of the entity.</summary>
-  string GetEntityNiceName(BaseComponent obj) {
-    string niceName;
-    if (obj.TryGetComponentFast<EntityComponent>(out var component)) {
-      niceName = _entityBadgeService.GetEntityName(component);
-    } else {
-      DebugEx.Error("Cannot get entity for: {0}", obj);
-      niceName = obj.GetComponentFast<Prefab>().Name.Split(new[] { '.' }, 2)[0];
-    }
-    return niceName;
   }
   #endregion
 }
--- Source/Tools/ResumeTool.cs
+++ Source/Tools/ResumeTool.cs
@@ -1,5 +1,4 @@
 using IFTTT_Automation.Utils;
-using TimberApi.ToolSystem;
 using Timberborn.BlockSystem;
 using Timberborn.BuildingsBlocking;
 using Timberborn.ToolSystem;
@@ -8,37 +7,54 @@ using UnityEngine;
 namespace IFTTT_Automation.Tools {
 
 // ReSharper disable once ClassNeverInstantiated.Global
-sealed class ResumeTool : AbstractAreaSelectionTool {
+sealed class ResumeTool : AbstractLockingTool {
+  #region Local fields and properties
+  const string ToolHintLoc = "IgorZ.Automation.ResumeTool.Hint";
+  #endregion
+
   #region Tool overrides
-  /// <inheritdoc/>
-  public override ToolDescription Description() {
-    var unused = Loc.T("blah");//FIXME
-    return new ToolDescription.Builder(Loc.T(ToolSpecification.NameLocKey))
-        .AddSection(Loc.T(ToolSpecification.DescriptionLocKey))
-        .AddPrioritizedSection(Loc.T("IgorZ.Automation.ResumeTool.Hint"))
-        .Build();
-  }
   #endregion
 
   #region CustomTool overrides
   /// <inheritdoc/>
-  public override void InitializeTool(ToolGroup toolGroup, ToolSpecification toolSpecification) {
+  protected override void Initialize() {
     HighlightColor = ActionColor = Color.green;
     TileColor = SideColor = Color.white;
-    base.InitializeTool(toolGroup, toolSpecification);
+    DescriptionHintSectionLoc = ToolHintLoc;
+    base.Initialize();
   }
   #endregion
 
   #region AbstractAreaSelectionTool overries
   /// <inheritdoc/>
   protected override bool ObjectFilterExpression(BlockObject blockObject) {
-    var component = blockObject.GetComponentFast<PausableBuilding>();
-    return component != null && component.enabled && component.IsPausable() && component.Paused;
+    if (!base.ObjectFilterExpression(blockObject)) {
+      return false;
+    }
+    var component = GetCompatibleComponent(blockObject);
+    return component != null && component.Paused;
   }
 
   /// <inheritdoc/>
   protected override void OnObjectAction(BlockObject blockObject) {
     blockObject.GetComponentFast<PausableBuilding>().Resume();
+  }
+  #endregion
+
+  #region AbstractLockingTool overries
+  /// <inheritdoc/>
+  protected override bool CheckCanLockOnComponent(BlockObject obj) {
+    return GetCompatibleComponent(obj) != null;
+  }
+  #endregion
+
+  #region Implementation
+  static PausableBuilding GetCompatibleComponent(BlockObject obj) {
+    var component = obj.GetComponentFast<PausableBuilding>();
+    if (component != null && component.enabled && component.IsPausable()) {
+      return component;
+    }
+    return null;
   }
   #endregion
 }
