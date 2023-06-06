e4c02e4d729439bf3a8bfb0a4f063f3f336b251d
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2023-06-05 21:51:07 -0700

    Use standard cancel cursor and better colors

--- Source/Tools/CancelTool.cs
+++ Source/Tools/CancelTool.cs
@@ -4,19 +4,33 @@
 
 using Automation.Utils;
 using Timberborn.BlockSystem;
+using UnityEngine;
 
 namespace Automation.Tools {
 
 // ReSharper disable once ClassNeverInstantiated.Global
-sealed class CancelTool : AbstractAreaSelectionTool {
+sealed class CancelTool : AbstractAreaSelectionTool, IAutomationModeEnabler {
+  #region CustomTool overrides
+  /// <inheritdoc/>
+  protected override void Initialize() {
+    SetColorSchema(Color.red, Color.red, Color.white, Color.white);
+    SetGameCursor("CancelCursor");
+    base.Initialize();
+  }
+  #endregion
+
+  #region AbstractAreaSelectionTool overrides
+  /// <inheritdoc/>
   protected override bool ObjectFilterExpression(BlockObject blockObject) {
     var automationBehavior = blockObject.GetComponentFast<AutomationBehavior>();
     return automationBehavior != null && automationBehavior.enabled && automationBehavior.HasRules;
   }
 
+  /// <inheritdoc/>
   protected override void OnObjectAction(BlockObject blockObject) {
     blockObject.GetComponentFast<AutomationBehavior>().ClearRules();
   }
+  #endregion
 }
 
 }
